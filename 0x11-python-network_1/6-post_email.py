76d60d05a59b106719f9d3fd81d3f07850436390
vee maiyo <Maiyonarukin@gmail.com>
2022-09-23 06:28:37 -0700

    search APAI

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Module for task 6 """
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response."""
+
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    import requests
-    import sys
-
-    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print(req.text)
+    print(requests.post(argv[1], data={'email': argv[2]}).text)
