7b9f39b768a7a76eab8fd3037b49acd4e7a83b9b
vee maiyo <Maiyonarukin@gmail.com>
2022-09-23 06:45:37 -0700

    what my status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Module for task 4 """
+"""Python script that fetches https://intranet.hbtn.io/status."""
+
+import requests
 
 if __name__ == "__main__":
-    import requests
-
-    req = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(req.text))
+    content = requests.get("https://intranet.hbtn.io/status").text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
