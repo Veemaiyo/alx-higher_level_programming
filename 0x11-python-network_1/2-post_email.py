d4a40540d573de73ab5b73f97ed059d140b7a874
vee maiyo <Maiyonarukin@gmail.com>
2022-09-23 04:08:30 -0700

    post an email#0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module for task 3 """
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
